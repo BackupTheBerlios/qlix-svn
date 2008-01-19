a208a9f417c6dbf711c75db0d1ab474eb15a2251
caffeinebot <caffeinebot@97db03ad-8736-0410-9762-8805f5232b6b>
2008-01-19 18:29:12 +0000

    "Added missing file"

---
+++ branches/Qlix2/types.h
@@ -0,0 +1,8 @@
+#ifndef __TYPES__
+#define __TYPES__
+#include <vector>
+using namespace std;
+typedef vector<LIBMTP_error_t> MTPErrorVector;
+typedef unsigned int count_t;
+typedef unsigned int index_t;
+#endif
